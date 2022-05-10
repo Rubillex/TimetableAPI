ac5bf3ae833b94a360112c4c9df53002ed7cbeb8
rubillex <gubchenko.vadim@gmail.com>
2022-05-10 18:45:26 +0700

    * промежуточный коммит

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,9 @@ Route::get('/timetable',                      [HomeController::class, 'index']);
 Route::post('/timetable/api/searchTimetable/{name}/{type}', [\App\Http\Controllers\TimetableController::class, 'searchTimetable']);
 Route::post('/timetable/api/getTimetable/{query}/{type}/{numGroup}', [\App\Http\Controllers\TimetableController::class, 'getTimetable']);
 
-
-Route::get('/phpInfo', function() {
-    return response()->json([
-        'stuff' => phpinfo()
-    ]);
-});
-
 Route::get('/reset-password/{token}', function ($token) {
     return view('auth.passwords.reset', ['token' => $token]);
 })->middleware('guest')->name('password.reset');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
